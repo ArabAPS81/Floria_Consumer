8408ce39ebd80f63de5f0f443f81c0978373229e
Obida <Abdelrhmana.elnasseral@gmail.com>
2019-11-18 10:51:32 +0200

    someUI1

---
+++ floriaConsumer/ProductCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCollectionViewCell.swift
+//  
+//
+//  Created by macbookpro on 7/15/19.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet var viewOfProductsImags: UIView!
+    @IBOutlet var image: UIImageView!
+    @IBOutlet weak var numberofrate: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var storename: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+}
