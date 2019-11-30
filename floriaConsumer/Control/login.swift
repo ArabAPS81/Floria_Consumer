1e7b42d260a53882eefaca12434ca0eab214e9e9
Obida <Abdelrhmana.elnasseral@gmail.com>
2019-11-30 03:43:09 +0200

    someUI after retet26

--- floriaConsumer/Control/login.swift
+++ floriaConsumer/Control/login.swift
@@ -12,7 +12,7 @@ class login: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        sign.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
     
@@ -20,22 +20,31 @@ class login: UIViewController {
   
     @IBOutlet weak var pass: UITextField!
     @IBOutlet weak var Done: UIScrollView!
+    @IBAction func signin(_ sender: Any) {
+    }
     
     
     @IBAction func skip(_ sender: Any) {
         self.performSegue(withIdentifier: "loged", sender: nil)
     }
+    @IBOutlet weak var sign: UIButton!
+    @IBAction func sho(_ sender: Any) {
+           if  pass.isSecureTextEntry == true
+           {
+              
+               pass.isSecureTextEntry=false
+            
+           }
+           else
+           {
+               
+               pass.isSecureTextEntry=true
+               
+           }
+       }
+    
  
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
    
     }
 
