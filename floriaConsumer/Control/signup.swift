116d14e0c43a11e79699f601ae39afb5be4330b9
Obida <Abdelrhmana.elnasseral@gmail.com>
2019-12-10 03:52:19 +0200

    fix and implement

--- floriaConsumer/Control/signup.swift
+++ floriaConsumer/Control/signup.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
 
 class signup: UIViewController {
 
@@ -28,6 +29,7 @@ class signup: UIViewController {
     
     @IBAction func signup(_ sender: Any) {
       
+        sign(name: name.text!, email: mobile.text!, password: pass.text!)
         
     }
    
@@ -62,5 +64,62 @@ class signup: UIViewController {
                    disshow.isHidden = true
               }
           }
-   
+   let url = "http://api2.floriaapp.com/api/v1/register"
+    func sign(name:String , email : String, password : String){
+               
+                 let parameter : Parameters =
+                     [
+                      
+                         "name" : name,
+                         "mobile" : email,
+                         "check_privacy" : "1",
+                         "password": password
+                 ]
+                 
+                 
+                 print(parameter,"909090900900009099090")
+                 Alamofire.request(self.url,method: .post, parameters: parameter, encoding: URLEncoding.default, headers: nil).responseJSON { re in
+                     switch re.result
+                     {
+                     case .failure(let erro):
+                         print("********////",erro)
+                     case .success(let value):
+                         var jsoncode = JSON(value)
+                         print(jsoncode,"00000000000000000")
+                 
+                         guard let data = jsoncode["data"].dictionary else  {self.alertt(header: "Alert ", body: "please check your data  ")
+                            return }
+                         print(data,"???????????????")
+                       
+                         let token = data["access_token"]?.string
+                             UserDefaults.standard.set(token, forKey: "tocken")
+                          
+                             
+                         }
+                         
+                     }
+                     
+                     
+                 }
+         
+         
+         /*
+         // MARK: - Navigation
+
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+         }
+         */
+         func alertt(header:String,body:String ) {
+                let alert = UIAlertController(title: header, message: body , preferredStyle: UIAlertController.Style.actionSheet)
+                                  
+                                  self.present(alert, animated: true, completion: nil)
+                              let when = DispatchTime.now() + 2
+                              DispatchQueue.main.asyncAfter(deadline: when){
+                                // your code with delay
+                                alert.dismiss(animated: true, completion: nil)
+                              }
+}
 }
