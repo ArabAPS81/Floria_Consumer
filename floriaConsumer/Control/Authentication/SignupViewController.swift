f521f7e3c2383e622904a657c18f78da04967567
labeeb <a.labib@arabaps.com>
2019-12-11 11:11:54 +0200

    sign up after merge conflicts

--- floriaConsumer/Control/Authentication/SignupViewController.swift
+++ floriaConsumer/Control/Authentication/SignupViewController.swift
@@ -1,5 +1,5 @@
 //
-//  signup.swift
+//  SignupViewController.swift
 //  floriaConsumer
 //
 //  Created by mac on 14/02/1441 AH.
@@ -7,25 +7,34 @@
 //
 
 import UIKit
-import Alamofire
-import SwiftyJSON
+
 
 class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        if UserDefaults.standard.string(forKey: "register") == "1"{
+            self.alertt(header: "Alert ", body: "please check your data  ")
+        }
+        super.viewDidLoad()
+        bu.layer.cornerRadius = 10
+        UserDefaults.standard.removeObject(forKey: "register")
+        // Do any additional setup after loading the view.
+    }
+  
     
-    
+
     @IBOutlet weak var confirm: UITextField!
     @IBOutlet weak var pass: UITextField!
     @IBOutlet weak var mail: UITextField!
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var mobile: UITextField!
     @IBOutlet weak var bu: UIButton!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+    
     @IBAction func signup(_ sender: Any) {
-        sign(name: name.text!, email: mobile.text!, password: pass.text!)
+      let vc = LoginService()
+      vc.sign(name: name.text!, email: mobile.text!, password: pass.text!, ext: "register")
+        
+        
     }
    
     @IBOutlet weak var checkbox: UIButton!
@@ -45,65 +54,39 @@ class SignupViewController: UIViewController {
     }
 
     @IBAction func sho(_ sender: Any) {
-        if  pass.isSecureTextEntry == true
-        {
-            
-            pass.isSecureTextEntry=false
-            disshow.isHidden = false
-            
-        }
-        else
-        {
-            
-            pass.isSecureTextEntry=true
-            disshow.isHidden = true
-        }
-    }
-   let url = "http://api2.floriaapp.com/api/v1/register"
-    func sign(name:String , email : String, password : String){
-        
-        let parameter : Parameters =
-            [
+              if  pass.isSecureTextEntry == true
+              {
+                 
+                  pass.isSecureTextEntry=false
+                disshow.isHidden = false
                 
-                "name" : name,
-                "mobile" : email,
-                "check_privacy" : "1",
-                "password": password
-        ]
-        
-        print(parameter,"909090900900009099090")
-        Alamofire.request(self.url,method: .post, parameters: parameter, encoding: URLEncoding.default, headers: nil).responseJSON { re in
-            switch re.result
-            {
-            case .failure(let erro):
-                print("********////",erro)
-            case .success(let value):
-                var jsoncode = JSON(value)
-                print(jsoncode,"00000000000000000")
-                
-                guard let data = jsoncode["data"].dictionary else  {self.alertt(header: "Alert ", body: "please check your data  ")
-                    return }
-                print(data,"???????????????")
-                
-                let token = jsoncode["data"]["access_token"].string
-                UserDefaults.standard.set(token, forKey: "token")
-                
-                
-            }
-            
-        }
-        
-        
-    }
+              }
+              else
+              {
+                  
+                  pass.isSecureTextEntry=true
+                   disshow.isHidden = true
+              }
+          }
+   
+         
+         /*
+         // MARK: - Navigation
 
-    func alertt(header:String,body:String ) {
-        let alert = UIAlertController(title: header, message: body , preferredStyle: UIAlertController.Style.actionSheet)
-        
-        self.present(alert, animated: true, completion: nil)
-        let when = DispatchTime.now() + 2
-        DispatchQueue.main.asyncAfter(deadline: when){
-            // your code with delay
-            alert.dismiss(animated: true, completion: nil)
-        }
-    }
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+         }
+         */
+         func alertt(header:String,body:String ) {
+                let alert = UIAlertController(title: header, message: body , preferredStyle: UIAlertController.Style.actionSheet)
+                                  
+                                  self.present(alert, animated: true, completion: nil)
+                              let when = DispatchTime.now() + 2
+                              DispatchQueue.main.asyncAfter(deadline: when){
+                                // your code with delay
+                                alert.dismiss(animated: true, completion: nil)
+                              }
+}
 }
